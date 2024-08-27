21140c89aadbb84ec52a626d87b7a0cab4d20629
kmarkus1 <167196765+kmarkus1@users.noreply.github.com>
2024-08-27 13:02:59 +0300

    Add files via upload

---
+++ Uppgifter/JavaUppgift3.java
@@ -0,0 +1,39 @@
+
+public class JavaUppgift3 {
+
+   
+    public static void main(String args[]) {
+        
+        //int to double
+        int myInt = 5;
+        double myDouble = myInt;
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        
+        //float to double
+        float myFloat = 6.99f;
+        double myDouble2 = myFloat;
+        System.out.println(myFloat);
+        System.out.println(myDouble2);
+        
+        //double to int
+        double myDouble3 = 8;
+        int myInt2 = (int)myDouble3;
+        System.out.println(myDouble3);
+        System.out.println(myInt2);
+        
+        
+        //char to int
+        char myChar = '2';
+        int myInt3 = myChar;
+        System.out.println(myChar);
+        System.out.println(myInt3);
+        
+        //int to byte
+        int myInt4 = 354;
+        byte myByte = (byte)myInt4;
+        System.out.println(myInt4);
+        System.out.println(myByte);
+        
+    }
+}
