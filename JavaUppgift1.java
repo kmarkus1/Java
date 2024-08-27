6c41d36cc7a2244caf48f581af32138e85180bbc
kmarkus1 <167196765+kmarkus1@users.noreply.github.com>
2024-08-27 09:10:55 +0300

    Add files via upload

---
+++ JavaUppgift1.java
@@ -0,0 +1,24 @@
+
+public class JavaUppgift1 {
+
+    public static void main(String args[]){
+        
+        String firstName = "Bob";
+        System.out.println("First Name: " + firstName);
+        
+        String lastName = "Bobston";
+        System.out.println("Last Name: " + lastName);
+        
+        int Age = 22;
+        System.out.println(Age);
+        
+        double Height = 180.7;
+        System.out.println(Height);
+       
+        boolean isAStudent = true;
+        System.out.println(isAStudent);
+        
+                
+    }
+ 
+}
